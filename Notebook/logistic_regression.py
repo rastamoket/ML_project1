6d4693478b001a1dfa4e045f3c28b7d2dcc77f74
iperret <ivan.perret@epfl.ch>
2017-10-20 12:04:56 +0200

    Implementation of Newton - not finished

--- Notebook/logistic_regression.py
+++ Notebook/logistic_regression.py
@@ -47,4 +47,46 @@ def learning_grad_descent(y, tx, initial_w, max_iters, gamma):
     
    
     return ws[-1], losses[-1]
+
+def calculate_hessian(y, tx, w):
+    """return the hessian of the loss function."""
+    dia=sigmoid(tx.dot(w))*(1-sigmoid(tx.dot(w)))
+    S=np.diag(dia.T[0])
+    return tx.T.dot(S.dot(tx))
+
+def learning_by_newton_method(y, tx, initial_w, max_iters):
+    """
+    Do one step on Newton's method.
+    return the loss and updated w.
+    """
+    loss, gradient, hessian=logistic_regression(y, tx, w)
+    w=w-np.linalg.inv(hessian).dot(gradient)
+    
+    
+        # init parameters
+    threshold = 1e-8
+    lambda_ = 0.1
+    losses = []
+
+    # build tx
+    tx = np.c_[np.ones((y.shape[0], 1)), x]
+    w = np.zeros((tx.shape[1], 1))
+
+    # start the logistic regression
+    for iter in range(max_iter):
+        # get loss and update w.
+        loss, w = learning_by_newton_method(y, tx, w)
+        # log info
+        if iter % 1 == 0:
+            print("Current iteration={i}, the loss={l}".format(i=iter, l=loss))
+        # converge criterion
+        losses.append(loss)
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+    # visualization
+    visualization(y, x, mean_x, std_x, w, "classification_by_logistic_regression_newton_method")
+    print("loss={l}".format(l=calculate_loss(y, tx, w)))
+    return loss, w
+
+
     
